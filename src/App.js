9c12e10f7a630f3fa3ef6aa32fa5822bbee36543
Victoria00000 <vvictoriaandres@gmail.com>
2022-02-02 06:59:49 -0300

    commit para guardar en github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
+import Atanagildo from './img/rey_atanagildo.png';
+import Ataulfo from './img/rey_ataulfo.png';
+import Ervigio from './img/rey_ervigio.png';
+import incognito from './img/rey_incognito.png';
 
 function App() {
+  let names = ['Atanagildo', 'Ataulfo', 'Ervigio'];
+
+  const cambioTexto = (evt) => evt.target.innerHTML === 'visto' ? evt.target.innerHTML = '' : evt.target.innerHTML = 'visto';
+  const cambioIncognito = (evt) => {
+    evt.target.src.includes('incognito') ? evt.target.style.visibility = 'hidden' : evt.target.src = incognito;
+    evt.target.parentNode.style.backgroundColor = 'white';
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='container_box'>
+        <img className='img' src={Atanagildo} onClick={cambioIncognito} />
+        <div className='names' onClick={cambioTexto}> {names[0]} </div>
+      </div>
+
+      <div className='container_box'>
+        <img className='img' src={Ataulfo} onClick={cambioIncognito} />
+        <div className='names' onClick={cambioTexto}> {names[1]} </div>
+      </div>
+
+      <div className='container_box'>
+        <img className='img' src={Ervigio} onClick={cambioIncognito} />
+        <div className='names' onClick={cambioTexto}> {names[2]} </div>
+      </div>
     </div>
   );
 }
